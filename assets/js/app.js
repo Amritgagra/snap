cfdabb24bffb73762ebab02bff3ce49104c06564
Amritgagra <amritgagra200@gmail.com>
2022-08-22 11:00:05 +0530

    updating dropdown code for js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileSidebar = document.querySelector('.list');
 const closeMenu = document.querySelector('.close-icon--items');
+const dropdownItems = document.querySelectorAll('.nav__list-dropdown-item');
+const dropdownList = document.querySelectorAll('.dropdown-list');
+
 
 mobileMenu.addEventListener('click',function(e){
     mobileSidebar.classList.add('show');
@@ -9,9 +12,19 @@ closeMenu.addEventListener('click',function(){
     mobileSidebar.classList.remove('show');
 })
 
+dropdownItems.forEach(item => {
+    item.addEventListener('click',function(){
+        dropdownList.forEach(listItem => {
+            listItem.classList.remove('active');
+        })
+    this.children[1].classList.add('active');
+    })
+})
+
 document.addEventListener('click',function(e){
-    if(e.target.matches('.list__link')){
-        const li = e.target.parentElement;
-        li.querySelector('.dropdown-list').classList.toggle('active');
-    }
+    dropdownItems.forEach(item =>{
+        if(!item.contains(e.target)){
+            item.children[1].classList.remove('active');
+        }
+    })
 })
